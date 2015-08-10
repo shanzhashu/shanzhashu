5057f26177d21ad62f8afb6346f5fc1afa7ff072
liuxiao <21808552@qq.com>
2015-08-10 12:35:30 +0800

    Backup supports zip file format for attachment.

--- blog/backup/backupme.py
+++ blog/backup/backupme.py
@@ -4,13 +4,19 @@ import MySQLdb
 import os
 import smtplib
 import email
+import time
 import datetime
+import mimetypes
+import subprocess
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email import Utils, Encoders
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-conn = MySQLdb.connect(host='localhost', user='root', passwd='mypassword', charset="utf8")
+conn = MySQLdb.connect(host='localhost', user='root', passwd='96gong1.com', charset="utf8")
 curs = conn.cursor(cursorclass = MySQLdb.cursors.DictCursor)
 curs.execute("SET NAMES utf8")
 conn.select_db('mydata')
@@ -41,19 +47,68 @@ for i in range(count):
 curs.close()
 conn.close()
 
+f = open('message.txt', 'w')
+f.write(body)
+f.flush
+f.close
+
+path=os.path.realpath(sys.path[0]) + '/message.txt'
+print path
+
+f=open(path, 'rb')
+s=f.readlines()
+#print s
+f.close
+
+returnCode = subprocess.call('./zipmsg.py')
+print 'returncode:', returnCode  
+
+time.sleep(5)
+
 today = datetime.date.today()
 
-msg = MIMEText(body, 'plain', 'utf-8')
+#msg = MIMEText(body, 'plain', 'utf-8')
+msg = MIMEMultipart()
+
 msg['From'] = 'shanzhashu@163.com'
 msg['To'] = 'liuxiaoshanzhashu@gmail.com'
 msg['Subject'] = 'Backup Package ' + today.strftime('%Y-%m-%d %H:%M:%S')
+
+f=open('./message.zip', 'rb')
+s=f.read()
+#print s
+f.close
+
+fd = file('./message.zip',"rb")
+mimetype,mimeencoding = mimetypes.guess_type('./message.zip')
+if mimeencoding or (mimetype is None):
+    mimetype = "application/octet-stream"
+maintype,subtype = mimetype.split("/")
+
+if maintype == "text":
+    att1 = MIMEText(fd.read(),_subtype = subtype)
+else:
+    att1 = MIMEBase(maintype,subtype)
+    att1.set_payload(fd.read())
+    Encoders.encode_base64(att1)
+att1.add_header("Content-Disposition","attachment",filename = "message.zip")
+fd.close()
+
+#att1 = MIMEText(open('./message.zip', 'rb').read(), 'base64', 'utf-8')
+#att1["Content-Type"] = 'application/octet-stream'
+#att1["Content-Disposition"] = 'attachment; filename="message.zip"'
+msg.attach(att1)
 
 smtp = smtplib.SMTP()
 smtp.connect("smtp.163.com", 25)
 smtp.login("shanzhashu@163.com", "mypassword")
 smtp.sendmail("shanzhashu@163.com", "liuxiaoshanzhashu@gmail.com", msg.as_string())
 smtp.quit()
+#print msg.as_string()
 print "Mail send Success. Record MaxId"
+
+os.remove("message.txt")
+os.remove("message.zip")
 
 lf = open('id.txt', 'w');
 lf.write(str(MaxId));
---
+++ blog/backup/zipmsg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding=utf-8
+import MySQLdb
+import os
+import smtplib
+import email
+import time
+import datetime
+import zipfile
+import mimetypes
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email import Utils, Encoders
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+z = zipfile.ZipFile('message.zip', 'w', zipfile.ZIP_DEFLATED)
+path=os.path.realpath(sys.path[0]) + '/message.txt'
+print path
+
+f=open(path, 'rb')
+s=f.readlines()
+#print s
+f.close
+
+z.write(path, 'message.txt')
+z.setpassword('1233211234567')
+z.close
+
+print "Zip Success."
+
