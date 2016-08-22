354edf453bc38f75a32655d61a2d25e768fade6d
LiuXiao <liuxiao950@pingan.com.cn>
2016-08-22 17:39:00 +0800

    Show Init IP and add WinSock2 lib.

--- PingFile/RecvPingFile/RPFUnit.dfm
+++ PingFile/RecvPingFile/RPFUnit.dfm
@@ -1,9 +1,9 @@
 object RPFForm: TRPFForm
-  Left = 269
-  Top = 288
+  Left = 335
+  Top = 236
   BorderStyle = bsDialog
   Caption = 'Receive File from Ping'
-  ClientHeight = 139
+  ClientHeight = 179
   ClientWidth = 411
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -12,6 +12,8 @@ object RPFForm: TRPFForm
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
   object lblDir: TLabel
@@ -20,6 +22,13 @@ object RPFForm: TRPFForm
     Width = 77
     Height = 13
     Caption = 'Store Directory:'
+  end
+  object lblRecvFrom: TLabel
+    Left = 16
+    Top = 56
+    Width = 68
+    Height = 13
+    Caption = 'Recv From IP:'
   end
   object edtDir: TEdit
     Left = 104
@@ -40,17 +49,26 @@ object RPFForm: TRPFForm
   end
   object btnRecv: TButton
     Left = 16
-    Top = 88
+    Top = 128
     Width = 369
     Height = 25
     Caption = 'Recv!'
     TabOrder = 2
+    OnClick = btnRecvClick
   end
   object pbRecv: TProgressBar
     Left = 16
-    Top = 56
+    Top = 96
     Width = 369
     Height = 16
     TabOrder = 3
+  end
+  object cbbIP: TComboBox
+    Left = 104
+    Top = 56
+    Width = 193
+    Height = 21
+    ItemHeight = 13
+    TabOrder = 4
   end
 end
--- PingFile/RecvPingFile/RPFUnit.pas
+++ PingFile/RecvPingFile/RPFUnit.pas
@@ -4,7 +4,24 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ComCtrls, StdCtrls, FileCtrl;
+  Dialogs, ComCtrls, StdCtrls, FileCtrl, Winsock2;
+
+const
+  ICMPDLL = 'icmp.dll';
+  WS2_32DLL = 'WS2_32.DLL';
+  MAXIPNOTE = 255;
+  IPJOIN = '.';
+  IPADDRFORMAT = '%0:D.%1:D.%2:D.%3:D';
+  SIO_GET_INTERFACE_LIST = $4004747F;
+  IFF_UP = $00000001;
+  IFF_BROADCAST = $00000002;
+  IFF_LOOPBACK = $00000004;
+  IFF_POINTTOPOINT = $00000008;
+  IFF_MULTICAST = $00000010;
+  IPNOTE1 = $FF000000;
+  IPNOTE2 = $00FF0000;
+  IPNOTE3 = $0000FF00;
+  IPNOTE4 = $000000FF;
 
 type
   TRPFForm = class(TForm)
@@ -13,13 +30,62 @@ type
     btnBrowse: TButton;
     btnRecv: TButton;
     pbRecv: TProgressBar;
+    lblRecvFrom: TLabel;
+    cbbIP: TComboBox;
     procedure btnBrowseClick(Sender: TObject);
+    procedure btnRecvClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     FRecving: Boolean;
+    FSnifSock: Integer;
     procedure UpdateButtonState;
+    procedure StartSniff;
+    procedure StopSniff;
   public
     { Public declarations }
   end;
+
+  TIpInfo = record
+    Address: Int64;
+    IP: string;
+    Host: string;
+  end;
+
+  TIP_NetType = (iptNone, iptANet, iptBNet, iptCNet, iptDNet, iptENet,
+    iptBroadCast, iptKeepAddr);
+
+  TIPNotes = array[1..4] of Byte;
+
+  TIP_Info = packed record
+    IPAddress: Cardinal;                 // IP地址,此处用整形存储
+    SubnetMask: Cardinal;                // 子网掩码,此处用整形存储
+    BroadCast: Cardinal;                 // 广播地址,此处用整形存储
+    HostName: array[0..255] of AnsiChar; // 主机名
+    NetType: TIP_NetType;                // IP地址的网络类型
+    Notes: TIPNotes;                     // IP地址的各子节点
+    UpState: Boolean;                    // 启用状态
+    Loopback: Boolean;                   // 是否环回地址
+    SupportBroadcast: Boolean;           // 是否支持广播
+  end;
+
+  TIPGroup = array of TIP_Info; //IP地址组
+
+  sockaddr_gen = packed record
+    AddressIn: sockaddr_in;
+    filler: packed array[0..7] of AnsiChar;
+  end;
+
+  TINTERFACE_INFO = packed record
+    iiFlags: u_long; // Interface flags
+    iiAddress: sockaddr_gen; // Interface address
+    iiBroadcastAddress: sockaddr_gen; // Broadcast address
+    iiNetmask: sockaddr_gen; // Network mask
+  end;
+
+  TWSAIoctl = function(s: TSocket; cmd: DWORD; lpInBuffer: PByte; dwInBufferLen:
+    DWORD; lpOutBuffer: PByte; dwOutBufferLen: DWORD; lpdwOutBytesReturned:
+    LPDWORD; lpOverLapped: POINTER; lpOverLappedRoutine: POINTER): Integer; stdcall;
 
 var
   RPFForm: TRPFForm;
@@ -30,6 +96,180 @@ implementation
 
 const
   SEL_DIR_CAPTION = 'Select a Directory:';
+  RECV_CAPTION = 'Recv!';
+  STOP_CAPTION = 'Stop!';
+
+var
+  WSAIoctl: TWSAIoctl = nil;
+  WS2_32DllHandle: THandle = 0;
+
+procedure InitWSAIoctl;
+begin
+  WS2_32DllHandle := LoadLibrary(WS2_32DLL);
+  if WS2_32DllHandle <> 0 then
+  begin
+    @WSAIoctl := GetProcAddress(WS2_32DllHandle, 'WSAIoctl');
+  end;
+end;
+
+procedure FreeWSAIoctl;
+begin
+  if WS2_32DllHandle <> 0 then
+    FreeLibrary(WS2_32DllHandle);
+end;
+
+function SetIP(aIPAddr: string; var aIP: TIpInfo): Boolean;
+var
+  pIPAddr: PAnsiChar;
+begin
+  Result := False;
+  aIP.Address := INADDR_NONE;
+  aIP.IP := aIPAddr;
+  aIP.Host := '';
+  if aIP.IP = '' then
+    Exit;
+
+  GetMem(pIPAddr, Length(aIP.IP) + 1);
+  try
+    ZeroMemory(pIPAddr, Length(aIP.IP) + 1);
+    StrPCopy(pIPAddr, {$IFDEF UNICODE}AnsiString{$ENDIF}(aIP.IP));
+    aIP.Address := inet_addr(PAnsiChar(pIPAddr)); // IP转换成无点整型
+  finally
+    FreeMem(pIPAddr); // 释放申请的动态内存
+  end;
+  Result := aIP.Address <> INADDR_NONE;
+end;
+
+function GetIPNotes(const aIP: string; var aResult: TIPNotes): Boolean;
+var
+  iPos, iNote: Integer;
+  sIP: string;
+
+  function CheckIpNote(aNote: string): Byte;
+  begin
+    iNote := StrToInt(aNote);
+    if (iNote < 0) or (iNote > MAXIPNOTE) then
+      raise Exception.Create(aNote + ' Error Range.');
+    Result := iNote;
+  end;
+
+begin
+  iPos := Pos(IPJOIN, aIP);
+  aResult[1] := CheckIpNote(Copy(aIP, 1, iPos - 1));
+  sIP := Copy(aIP, iPos + 1, 20);
+  iPos := Pos(IPJOIN, sIP);
+  aResult[2] := CheckIpNote(Copy(sIP, 1, iPos - 1));
+  sIP := Copy(sIP, iPos + 1, 20);
+  iPos := Pos(IPJOIN, sIP);
+  aResult[3] := CheckIpNote(Copy(sIP, 1, iPos - 1));
+  aResult[4] := CheckIpNote(Copy(sIP, iPos + 1, 20));
+  Result := aResult[1] > 0;
+end;
+
+function IPTypeCheck(const aIP: string): TIP_NetType;
+var
+  FNotes: TIPNotes;
+begin
+  Result := iptNone;
+  if GetIPNotes(aIP, FNotes) then
+  begin
+    case FNotes[1] of
+      1..126:
+        Result := iptANet;
+      127:
+        Result := iptKeepAddr;
+      128..191:
+        Result := iptBNet;
+      192..223:
+        Result := iptCNet;
+      224..239:
+        Result := iptDNet;
+      240..255:
+        Result := iptENet;
+    else
+      Result := iptNone;
+    end;
+  end;
+end;
+
+function IPToInt(const aIP: string): Cardinal;
+var
+  FNotes: TIPNotes;
+begin
+  Result := 0;
+  if IPTypeCheck(aIP) = iptNone then
+  begin
+    //raise Exception.Create(SCnErrorAddress);
+    Exit;
+  end;
+  if GetIPNotes(aIP, FNotes) then
+  begin
+    Result := Result or FNotes[1] shl 24 or FNotes[2] shl 16 or FNotes[3] shl 8
+      or FNotes[4];
+  end;
+end;
+
+function IntToIP(const aIP: Cardinal): string;
+var
+  FNotes: TIPNotes;
+begin
+  FNotes[1] := aIP and IPNOTE1 shr 24;
+  FNotes[2] := aIP and IPNOTE2 shr 16;
+  FNotes[3] := aIP and IPNOTE3 shr 8;
+  FNotes[4] := aIP and IPNOTE4;
+  Result := Format(IPADDRFORMAT, [FNotes[1], FNotes[2], FNotes[3], FNotes[4]]);
+end;
+
+function EnumLocalIP(var aLocalIP: TIPGroup): Integer;
+var
+  skLocal: TSocket;
+  iIP: Integer;
+  PtrA: pointer;
+  BytesReturned, SetFlags: u_long;
+  pAddrInet: Sockaddr_IN;
+  Buffer: array[0..20] of TINTERFACE_INFO;
+  FWSAData: TWSAData;
+begin
+  Result := 0;
+
+  WSAStartup($101, FWSAData);
+  try
+    skLocal := Socket(AF_INET, SOCK_STREAM, 0); // Open a socket
+    if (skLocal = INVALID_SOCKET) then
+      Exit;
+
+    try // Call WSAIoCtl
+      PtrA := @bytesReturned;
+      if (WSAIoCtl(skLocal, SIO_GET_INTERFACE_LIST, nil, 0, @Buffer, 1024, PtrA,
+        nil, nil) <> SOCKET_ERROR) then
+      begin // If ok, find out how
+        Result := BytesReturned div SizeOf(TINTERFACE_INFO);
+        SetLength(aLocalIP, Result);
+        for iIP := 0 to Result - 1 do // For every interface
+        begin
+          pAddrInet := Buffer[iIP].iiAddress.AddressIn;
+          aLocalIP[iIP].IPAddress := IPToInt({$IFDEF UNICODE}string{$ENDIF}(inet_ntoa
+            (pAddrInet.sin_addr)));
+          pAddrInet := Buffer[iIP].iiNetMask.AddressIn;
+          aLocalIP[iIP].SubnetMask := IPToInt({$IFDEF UNICODE}string{$ENDIF}(inet_ntoa
+            (pAddrInet.sin_addr)));
+          pAddrInet := Buffer[iIP].iiBroadCastAddress.AddressIn;
+          aLocalIP[iIP].BroadCast := IPToInt({$IFDEF UNICODE}string{$ENDIF}(inet_ntoa
+            (pAddrInet.sin_addr)));
+          SetFlags := Buffer[iIP].iiFlags;
+          aLocalIP[iIP].UpState := (SetFlags and IFF_UP) = IFF_UP;
+          aLocalIP[iIP].Loopback := (SetFlags and IFF_LOOPBACK) = IFF_LOOPBACK;
+          aLocalIP[iIP].SupportBroadcast := (SetFlags and IFF_BROADCAST) = IFF_BROADCAST;
+        end;
+      end;
+    except
+      ;
+    end;
+    CloseSocket(skLocal);
+  finally
+    WSACleanUp;
+  end;
+end;
 
 procedure TRPFForm.btnBrowseClick(Sender: TObject);
 var
@@ -42,7 +282,72 @@ end;
 
 procedure TRPFForm.UpdateButtonState;
 begin
+  if FRecving then
+    btnRecv.Caption := STOP_CAPTION
+  else
+    btnRecv.Caption := RECV_CAPTION;
+end;
+
+procedure TRPFForm.btnRecvClick(Sender: TObject);
+begin
+  if FRecving then
+  begin
+    // Stop
+    StopSniff;
+    FRecving := False;
+    UpdateButtonState;
+  end
+  else
+  begin
+    // Start
+    StartSniff;
+    FRecving := True;
+    UpdateButtonState;
+  end;
+
+end;
+
+procedure TRPFForm.FormCreate(Sender: TObject);
+var
+  Wsa: TWSAData;
+  aLocalIP: TIPGroup;
+  I: Integer;
+begin
+  if (WSAStartup(MAKEWORD(2, 2), Wsa) <> 0) then
+    raise Exception.Create('WSAStartup');
+  InitWSAIoctl;
+
+  EnumLocalIP(aLocalIP);
+  if Length(aLocalIP) > 0 then
+  begin
+    for I := 0 to Length(aLocalIP) - 1 do
+      cbbIP.Items.Add(IntToIP(aLocalIP[I].IPAddress));
+    cbbIP.ItemIndex := 0;
+  end;
+end;
+
+procedure TRPFForm.FormDestroy(Sender: TObject);
+begin
+  WSACleanup;
+  FreeWSAIoctl;
+end;
+
+procedure TRPFForm.StartSniff;
+begin
+  if FRecving then
+    Exit;
+
+
+end;
+
+procedure TRPFForm.StopSniff;
+begin
+  if not FRecving then
+    Exit;
+
 
 end;
 
 end.
+
+
